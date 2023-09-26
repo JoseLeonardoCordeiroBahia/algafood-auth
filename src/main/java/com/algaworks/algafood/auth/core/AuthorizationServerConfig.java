83fc668700e46fb831c5b6d389cd88d6caa564b3
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-09-26 16:13:49 -0300

    Colocando o nome dos escopos em maiúsculo

--- src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -46,7 +46,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.withClient("algafood-web")
 				.secret(passwordEncoder.encode("web123"))
 				.authorizedGrantTypes("password", "refresh_token")
-				.scopes("write", "read")
+				.scopes("WRITE", "READ")
 				.accessTokenValiditySeconds(6 * 60 * 60) // 6 horas
 				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 				
@@ -54,20 +54,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.withClient("foodanalytics")
 				.secret(passwordEncoder.encode(""))
 				.authorizedGrantTypes("authorization_code")
-				.scopes("write", "read")
+				.scopes("WRITE", "READ")
 				.redirectUris("http://localhost:8082")
 				
 			.and()
 				.withClient("webadmin")
 				.authorizedGrantTypes("implicit")
-				.scopes("write", "read")
+				.scopes("WRITE", "READ")
 				.redirectUris("http://aplicaco-cliente")
 				
 			.and()
 				.withClient("faturamento")
 				.secret(passwordEncoder.encode("faturamento123"))
 				.authorizedGrantTypes("client_credentials")
-				.scopes("read")
+				.scopes("READ")
 				
 			.and()
 				.withClient("checktoken")
